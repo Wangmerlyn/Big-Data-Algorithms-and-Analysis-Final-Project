1515352fccd5c08da74c742841d0572008846eb5
Wangmerlyn <wsy0227@sjtu.edu.cn>
2023-12-27 18:35:21 +0800

    add data_handlers

---
+++ data_handlers/train_test_split.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+
+
+def stratified_k_fold_split(
+    train_index, y, n_splits=5, shuffle=True, random_state=42
+):
+    skf = StratifiedKFold(
+        n_splits=n_splits, shuffle=shuffle, random_state=random_state
+    )
+    for train_index, test_index in skf.split(train_index, y):
+        yield train_index, test_index
