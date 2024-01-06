7512f69fe5c4ceaba3c8d1db275fc92b07d8a730
Wangmerlyn <wsy0227@sjtu.edu.cn>
2024-01-06 17:42:08 +0800

    test run

--- configs/model_config.py
+++ configs/model_config.py
@@ -2,7 +2,7 @@ configs = {
     "vanilla_gcn_cora_config": {"hidden_channels": 16, "dropout": 0.5},
     "vanilla_gat_cora_config": {
         "hidden_channels": 16,
-        "num_heads": 8,
+        "attention_heads": 8,
         "dropout": 0.6,
     },
     "gfusion_cora_config": {
@@ -12,9 +12,22 @@ configs = {
     },
 }
 
+model_key = {
+    "VanillaGCN": "vanilla_gcn",
+    "VanillaGAT": "vanilla_gat",
+    "GFusion": "gfusion",
+}
+
+dataset_key = {
+    "Cora": "cora",
+    "CiteSeer": "citeseer",
+}
+
 
 def get_config(model, dataset):
-    key = f"{model.lower()}_{dataset.lower()}_config"
+    assert model in model_key, f"Model {model} not found!"
+    assert dataset in dataset_key, f"Dataset {dataset} not found!"
+    key = f"{model_key[model]}_{dataset_key[dataset]}_config"
     assert key in configs, f"Config for {model} on {dataset} not found!"
     return configs[key]
 
